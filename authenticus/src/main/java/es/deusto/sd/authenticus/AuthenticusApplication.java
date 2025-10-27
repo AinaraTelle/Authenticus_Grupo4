5626101492f1e6a3dbd78e7e64dc587b8a71f4c3
Ainara Tellechea <a.tellechea@opendeusto.es>
2025-10-27 11:18:11 +0100

    Inicio proyecto

---
+++ authenticus/src/main/java/es/deusto/sd/authenticus/AuthenticusApplication.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.authenticus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticusApplication.class, args);
+	}
+
+}
---
+++ authenticus/src/test/java/es/deusto/sd/authenticus/AuthenticusApplicationTests.java
@@ -0,0 +1,13 @@
+package es.deusto.sd.authenticus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
