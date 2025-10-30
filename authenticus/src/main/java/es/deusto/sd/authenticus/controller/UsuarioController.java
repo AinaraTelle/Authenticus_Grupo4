17977fd2e8aa4f5cbfe3f5890d9f1fe2271c4afe
Ainara Tellechea <a.tellechea@opendeusto.es>
2025-10-31 00:05:36 +0100

    Importe de usuarios

--- authenticus/src/main/java/es/deusto/sd/authenticus/controller/UsuarioController.java
+++ authenticus/src/main/java/es/deusto/sd/authenticus/controller/UsuarioController.java
@@ -1,25 +1,24 @@
 package es.deusto.sd.authenticus.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+// import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.deusto.sd.authenticus.dto.UsuarioDTO;
 import es.deusto.sd.authenticus.service.Estado;
-// import es.deusto.sd.italianrestaurantclass.dto.OrderDTO;
-// import es.deusto.sd.italianrestaurantclass.entity.Order.OrderStatus;
-// import es.deusto.sd.italianrestaurantclass.service.OrderService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+// import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -31,5 +30,16 @@ public class UsuarioController {
 
     public UsuarioController(Estado estado){
         this.estado=estado;
+    }
+
+    @Operation(
+        summary = "Get Todos los usuarios",
+        description = "Te da la info de todos lo usuaios"
+    )
+    @ApiResponse(responseCode = "200", description = "Successful operation")
+    @GetMapping
+    public ResponseEntity <List<UsuarioDTO>> getAllUsuarios(){
+        ArrayList<UsuarioDTO> usuarios =estado.getAllUsuarios();
+        return new ResponseEntity<>(usuarios,HttpStatus.OK);
     }
 }
--- authenticus/src/main/java/es/deusto/sd/authenticus/service/Estado.java
+++ authenticus/src/main/java/es/deusto/sd/authenticus/service/Estado.java
@@ -1,43 +1,36 @@
 package es.deusto.sd.authenticus.service;
 import java.util.ArrayList;
-import java.util.HashMap;
+// import java.util.HashMap;
 
-import es.deusto.sd.authenticus.entity.Caso;
+import org.springframework.stereotype.Service;
+
+// import es.deusto.sd.authenticus.entity.Caso;
 import es.deusto.sd.authenticus.entity.Usuario;
+import es.deusto.sd.authenticus.dto.UsuarioDTO;
 
+@Service
 public class Estado {
-    ArrayList<Usuario> listUsersLogIn = new ArrayList<Usuario>();
-    ArrayList<Usuario> listUsersLogOut = new ArrayList<Usuario>();
+    private final ArrayList<Usuario> listUsersLogIn = new ArrayList<Usuario>();
+    // private final ArrayList<Usuario> listUsersLogOut = new ArrayList<Usuario>();
 
-    ArrayList< HashMap< Usuario,String>> listMap_UserToken= new ArrayList< HashMap< Usuario,String>>();
-    ArrayList< HashMap< Usuario,ArrayList<Caso>>> listMap_UserCases= new ArrayList< HashMap< Usuario,ArrayList<Caso>>>();
-
-    public ArrayList<Usuario> getListUsersLogIn() {
-        return listUsersLogIn;
-    }
-    public ArrayList<Usuario> getListUsersLogOut() {
-        return listUsersLogOut;
-    }
-    public ArrayList<HashMap<Usuario, String>> getListMap_UserToken() {
-        return listMap_UserToken;
-    }
-    public ArrayList<HashMap<Usuario, ArrayList<Caso>>> getListMap_UserCases() {
-        return listMap_UserCases;
-    }
-    public void setListUsersLogIn(ArrayList<Usuario> listUsersLogIn) {
-        this.listUsersLogIn = listUsersLogIn;
-    }
-    public void setListUsersLogOut(ArrayList<Usuario> listUsersLogOut) {
-        this.listUsersLogOut = listUsersLogOut;
-    }
-    public void setListMap_UserToken(ArrayList<HashMap<Usuario, String>> listMap_UserToken) {
-        this.listMap_UserToken = listMap_UserToken;
-    }
-    public void setListMap_UserCases(ArrayList<HashMap<Usuario, ArrayList<Caso>>> listMap_UserCases) {
-        this.listMap_UserCases = listMap_UserCases;
-    }    
+    // private final ArrayList< HashMap< Usuario,String>> listMap_UserToken= new ArrayList< HashMap< Usuario,String>>();
+    // private final ArrayList< HashMap< Usuario,ArrayList<Caso>>> listMap_UserCases= new ArrayList< HashMap< Usuario,ArrayList<Caso>>>();
 
     
 
+    public ArrayList<UsuarioDTO> getAllUsuarios(){
+        ArrayList<UsuarioDTO> listUsuariosDTOs = new ArrayList<UsuarioDTO>();
+        
+        for(Usuario usuario1: listUsersLogIn){
+            listUsuariosDTOs.add(convertToDTO(usuario1));
+        }
+        return listUsuariosDTOs;
+    }
+
+    private UsuarioDTO convertToDTO(Usuario usuario) {
+        return new UsuarioDTO(usuario.getIDUsuario(), 
+        usuario.getNombre(), usuario.getEmail(),
+        usuario.getPassword(),usuario.getTel());
+    }
 
 }
